7b917ca02bcd9cf2fbe802ec6fe860685c589d0c
john williams <johnwilliams1756@gmail.com>
2022-08-01 20:07:46 +0300

    search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,17 @@ import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 500);
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -13,17 +23,30 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
             setResults(data.query.search);
         };
-        setTimeout(() => {
-            if (term) {
-                search();
-            }
-        }, 500);
-    }, [term]);
+        search();
+    }, [debouncedTerm]);
+
+    // useEffect(() => {
+
+    //     if (term && !results.length) {
+    //         search();
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();
+    //             }
+    //         }, 500);
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         };
+    //     }
+
+    // }, [term]);
     const renderedResults = results.map(result => {
         return (
             <div key={result.pageid} className="item">
