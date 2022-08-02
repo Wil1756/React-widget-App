270f23cc9715217a4f5135447557a1a984d6eef0
john williams <johnwilliams1756@gmail.com>
2022-08-02 06:37:07 +0300

    translate widget

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 
 const options = [
@@ -13,7 +14,19 @@ const options = [
     },
     {
         label: 'Hindi',
-        value: 'hi'
+        value: 'hi',
+    },
+    {
+        label: 'French',
+        value: 'fr',
+    },
+    {
+        label: 'Croatian',
+        value: 'hr',
+    },
+    {
+        label: 'English',
+        value: 'en'
     }
 ];
 const Translate = () => {
@@ -37,6 +50,9 @@ const Translate = () => {
                 onSelectedChange={setLanguage}
                 options={options}
             />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 };
